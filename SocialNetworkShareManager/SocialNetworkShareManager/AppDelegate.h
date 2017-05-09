72d9aebc616b7187eb694c2460bb6780590d059f
wy1402099772 <1402099772@qq.com>
2017-05-09 18:15:22 +0800

    Init Project

---
+++ SocialNetworkShareManager/SocialNetworkShareManager/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SocialNetworkShareManager
+//
+//  Created by wyan assert on 09/05/2017.
+//  Copyright © 2017 wyan assert. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SocialNetworkShareManager/SocialNetworkShareManager/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SocialNetworkShareManager
+//
+//  Created by wyan assert on 09/05/2017.
+//  Copyright © 2017 wyan assert. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
